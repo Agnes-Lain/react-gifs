a3dd941936724e408accbf0c12445642d4979132
Yizhu ZONG <agnes.lain@gmail.com>
2020-12-30 02:00:42 +0100

    the click to show gif now is done

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Gif from "./gif";
 
 class GifList extends React.Component {
-  render() {
 
+  renderList = () => {
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} select={this.props.selectId} />);
+  }
+
+  render() {
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
